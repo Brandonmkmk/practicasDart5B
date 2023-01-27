84fce1656845ea87aae67ad7139eac3acdf785d5
Adrianxx1 <triaxis12q@gmail.com>
2023-01-27 11:41:39 -0600

    clases-abstractas

---
+++ clases-abstractas.dart
@@ -0,0 +1,26 @@
+void main() {
+  final primerCuadrado = Cuadrado();
+  print('el area del cuadrado es: ${primerCuadrado.area}');
+}
+
+abstract class FiguraGeometrica {
+  //estableciendo lo que debe contener una figura geometrica
+
+  double? area;
+
+  double calcularArea();
+}
+
+class Cuadrado implements FiguraGeometrica {
+  //cuadrado hereda de figura geometrica
+  // y debe implementar todo lo definido en la clase abstracta
+  @override
+  double? area;
+  @override
+  //se eusa override ya que se estan sobreescribiendo lo que ya estaba definido
+  // en la clase abstracta FiguraGeometrica
+  double calcularArea() {
+    double lado = 12.5;
+    return lado * lado;
+  }
+}
